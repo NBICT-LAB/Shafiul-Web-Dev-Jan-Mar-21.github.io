772a55e20e2c37e5e8dde3af6b3bd2c5bea7d108
Shafiul19 <78022633+Shafiul19@users.noreply.github.com>
2021-02-14 04:00:13 -0800

    js practice

--- extra/practice.js
+++ extra/practice.js
@@ -9,6 +9,18 @@ console.log(firstName+ ' ' +lastName+
 
 
 let x;
+let country = 'Bangladesh';
+let population = 160 ;
+let continet = 'Asia';
+let isIsland = true;
+let language = 'Bangla';
+console.log(country);
+console.log(continet);
 console.log(typeof(x));
+console.log(typeof(population));
+console.log(typeof(isIsland));
+console.log(typeof(language));
+
+
 
 
