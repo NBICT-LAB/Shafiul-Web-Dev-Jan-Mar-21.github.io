70d9152a192b23a2ac04d544f988177103ea10bd
Shafiul19 <78022633+Shafiul19@users.noreply.github.com>
2021-02-14 03:39:22 -0800

    jsfundamental first class

---
+++ extra/practice.js
@@ -0,0 +1,14 @@
+let js = 'awesome';
+console.log(js);
+console.log(7-11+19*2);
+let firstName ='Shafiul';
+let lastName  ='Alam';
+console.log(firstName+ ' ' +lastName+
+
+'is an cse student ');
+
+
+let x;
+console.log(typeof(x));
+
+
