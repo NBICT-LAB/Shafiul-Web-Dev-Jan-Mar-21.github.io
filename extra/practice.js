d8d44a86829241d3debaff63635a5d6713dc6ccf
Shafiul19 <shafiulhstu@gmail.com>
2021-03-03 20:58:30 -0800

    practice 03

--- extra/practice.js
+++ extra/practice.js
@@ -1,4 +1,4 @@
-let js = 'awesome';
+/*let js = 'awesome';
 console.log(js);
 console.log(7-11+19*2);
 let firstName ='Shafiul';
@@ -21,8 +21,42 @@ console.log(typeof(population));
 console.log(typeof(isIsland));
 console.log(typeof(language));
 const myName = 'Shafiul';
-console.log(myName);
+console.log(myName);*/
+const country = 'bangladesh';
+const population = 160;
+const continent = 'Asia';
+console.log(country,population,continent);
+const isIsland = true;
+const language = 'Bangla';
+console.log(`Type of 'country' is ${typeof(country)}.`);
+console.log(`Type of 'country' is ${typeof(language)}.`);
+console.log(`Type of 'country' is ${typeof(continent)}.`);
 
+let firstHalf = population/2;
+let secondHalf = population - firstHalf;
+console.log(`Each half will contain ${secondHalf} million people.`);
+let newPop = population+1;
+console.log(`if we add to 1 ${population} than we will get ${newPop}.`);
+let finPop = 19;
+let condion = population>finPop;
+if(condion){
+    console.log(`our population is greater than Finlands population`);
+}else{
+    console.log(`our population is less than Finland population`);
+}
 
+let averagePop = 33;
+if(population>averagePop){
+    console.log(`our population is greater than average population`);
+}else{
+    console.log(`our population is 
+    less than average population`);
+}
 
-
+let AkkasAge = 21;
+let ShuvoAge = 23;
+if(ShuvoAge>AkkasAge){
+    console.log(`Shuvo's age is greater than Akkas's age.`);
+}else{
+    console.log(`Akkas's age is greater than Shuvo's age.`);
+}
